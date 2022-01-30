ef29fd8fd1a9f176085ffb462ec43276c42d7baf
Ryan Uzumaki <ryandx0de@outlook.com>
2022-01-30 11:02:47 +0800

    加入注释

--- my_quickdemo.cpp
+++ my_quickdemo.cpp
@@ -24,7 +24,7 @@ void QuickDemo::image_create(Mat& image) {
 	Mat m1, m2;
 	//two methods of copying a image
 	m1 = image.clone();
-	image.copyTo(m2);
+	image.copyTo(m2);//还有一个mask参数，后面掩膜操作会用到
 
 	Mat m3;
 	m3 = Mat::zeros(Size(40,40), CV_8UC3);
@@ -47,7 +47,7 @@ void QuickDemo::pixel_visit(Mat& image) {
 				image.at<uchar>(row, col) = 255 - pv;
 			}
 			else if (dims == 3) {
-				Vec3b bgr = image.at<Vec3b>(row, col);
+				Vec3b bgr = image.at<Vec3b>(row, col);//Vec3b格式适用于一次提取三个数据组成数组
 				image.at<Vec3b>(row, col)[0] = 255 - bgr[0];	
 				image.at<Vec3b>(row, col)[1] = 255 - bgr[1];
 				image.at<Vec3b>(row, col)[2] = 255 - bgr[2];
@@ -62,7 +62,8 @@ void QuickDemo::operators_demo(Mat& image) {
 	Mat dst = Mat::zeros(image.size(), image.type());
 	Mat m = Mat::zeros(image.size(), image.type());
 	m = Scalar(50, 50, 50);
-	/*adding principle(can be used to enhance the luminance):
+	/*这里是add函数的内部实现部分，在使用时直接调用add的API
+	adding principle(can be used to enhance the luminance):
 	int w = image.cols;
 	int h = image.rows;
 	int dims = image.channels();
@@ -76,7 +77,7 @@ void QuickDemo::operators_demo(Mat& image) {
 		}
 	}
 	*/
-	subtract(image, m, dst);
+	subtract(image, m, dst);//减法dst=image-m本质上此条语句是在调整亮度
 	/*
 	corresponding to the +&-&*&/ operator:
 	add(image, m, dst);
@@ -88,6 +89,7 @@ void QuickDemo::operators_demo(Mat& image) {
 }
 
 //the "on_lightness" & "on_contrast" function can add a "static" before "void"
+//回调函数
 void on_lightness(int b, void* userdata) {
 	Mat image = *((Mat*)userdata);
 	Mat dst = Mat::zeros(image.size(), image.type());
@@ -96,12 +98,14 @@ void on_lightness(int b, void* userdata) {
 	imshow("亮度与对比度调整", dst);
 }
 
+//回调函数
 void on_contrast(int b, void* userdata) {
-	Mat image = *((Mat*)userdata);
+	Mat image = *((Mat*)userdata);//进行Mat类型指针强制转换
 	Mat dst = Mat::zeros(image.size(), image.type());
 	Mat m = Mat::zeros(image.size(), image.type());
 	double contrast = b / 100.0;
 	addWeighted(image, contrast, m, 0.0, 0, dst);
+	//https://blog.csdn.net/fanjiule/article/details/81607873?ops_request_misc=%257B%2522request%255Fid%2522%253A%2522164351142216780271589474%2522%252C%2522scm%2522%253A%252220140713.130102334..%2522%257D&request_id=164351142216780271589474&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~all~top_positive~default-1-81607873.pc_search_result_control_group&utm_term=addWeighted&spm=1018.2226.3001.4187
 	imshow("亮度与对比度调整", dst);
 }
 
@@ -110,6 +114,7 @@ void QuickDemo::tracking_bar_demo(Mat& image) {
 	int max_value = 100;
 	int contrast_value = 100;
 	namedWindow("亮度与对比度调整", WINDOW_FREERATIO);
+	//每当trackbar的数值条有变动的时候就会调用回调函数，而回调函数中的内容就是要对所接受的数据的操作
 	createTrackbar("Value Bar:", "亮度与对比度调整", 
 		&lightness, max_value, on_lightness, (void*)(&image));
 	createTrackbar("Contrast Bar:", "亮度与对比度调整", 
@@ -117,6 +122,7 @@ void QuickDemo::tracking_bar_demo(Mat& image) {
 	on_lightness(50, &image);
 }
 
+//与键盘交互
 void QuickDemo::key_respond(Mat& image) {
 	Mat dst=Mat::zeros(image.size(),image.type());
 	while (true) {
@@ -141,6 +147,7 @@ void QuickDemo::key_respond(Mat& image) {
 	}
 }
 
+//opencv中的自带滤镜库运用
 void QuickDemo::color_style_demo(Mat& image) {
 	int colormap[] = {
 		COLORMAP_AUTUMN,
@@ -178,19 +185,24 @@ void QuickDemo::color_style_demo(Mat& image) {
 	}
 }
 
+//对图像的位操作
 void QuickDemo::bitwise_demo(Mat& image) {
 	Mat m1 = Mat::zeros(Size(10, 10), CV_8UC3);
 	Mat m2 = Mat::zeros(Size(10, 10), CV_8UC3);
+	//CV_8UC3意思是八位无符号整数三通道格式（用imread函数读取的都是八位格式，通道数可以指定1/2/3)
 	Mat dst;
 	rectangle(m1, Rect(0, 0, 5, 5), Scalar(255, 255, 0), -1, LINE_8, 0);
 	rectangle(m2, Rect(3, 3, 5, 5), Scalar(0, 100, 254), -1, LINE_8, 0);
+	/*rectangle函数得倒数第三个参数小于零表示填充，大于零表示绘制边
+	Rect(x,y,width,height)，x, y为左上角坐标,width, height则为长和宽*/
 	//imshow("m1", m1);
 	//imshow("m2", m2);
-	bitwise_xor(m1, m2, dst);//bitwise_or,bitwise_and
+	bitwise_xor(m1, m2, dst);//还有bitwise_or,bitwise_and，bitwise_not操作
 	cout << dst << endl;
 	imshow("dst", dst);
 }
 
+//通道分离和通道混合操作
 void QuickDemo::channels_demo(Mat& image) {
 	vector<Mat> mv;
 	split(image, mv);//channels seperated
@@ -204,7 +216,7 @@ void QuickDemo::channels_demo(Mat& image) {
 	merge(mv, dst);
 	imshow("红色", dst);
 
-	int from_to[] = { 0, 2, 1,1, 2, 0 };
+	int from_to[] = { 0, 2, 1,1, 2, 0 };//0-->2,1-->1,2-->0
 	/*
 	input matrix address, input matrix number, output matrix address, 
 	output matrix number,mixed list,mixed channels pairs number
@@ -213,6 +225,7 @@ void QuickDemo::channels_demo(Mat& image) {
 	imshow("通道混合", dst);
 }
 
+//掩膜操作更换背景
 void QuickDemo::inrange_demo(Mat& image) {
 	Mat hsv;
 	cvtColor(image, hsv, COLOR_BGR2HSV);
@@ -230,6 +243,7 @@ void QuickDemo::inrange_demo(Mat& image) {
 	imshow("after_mask", back);
 }
 
+//图像数据统计
 void QuickDemo::pixel_statistic_demo(Mat& image) {
 	double minv, maxv;
 	Point minLoc, maxLoc;
